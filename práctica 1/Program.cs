f7343a204e32ece3aeb46d11fa4c937bde111725
elmag <elmag@DESKTOP-QHJH7K5>
2022-07-29 09:51:18 -0600

    primer programa

---
+++ práctica 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace práctica_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaración de variables
+            int num1 = 5;
+            int num2 = 4;
+            var total = num1 + num2;
+            
+            string name = "Darien";
+            int edad = 22;
+            // Impresión de datos
+            Console.WriteLine("La suma es: " + total);
+            Console.WriteLine(name + " " + edad);
+        }
+    }
+}

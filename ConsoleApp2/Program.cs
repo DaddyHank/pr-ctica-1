c9035fcf341e7a733729f2bdc5bff74d9e18ee65
elmag <elmag@DESKTOP-QHJH7K5>
2022-08-15 21:54:49 -0600

    practica 1

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaración de variables
+            string firstname = "Darien";
+            String lastname = "Molina";
+            int edad = 22;
+            // Concatenación de variables
+            var name = firstname + " " + lastname;
+            // imprimir
+            Console.WriteLine("Su nombre es: {0} y su edad es: {1}", name, edad);
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Suma
+            int num1, num2;
+            Console.WriteLine("Introduzca un número:");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Iintroduzca otro número:");
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("La suma de los números es: " + (num1 + num2));
+
+            // Resta
+            int num3, num4;
+            Console.WriteLine("introduzca un número:");
+            num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca otro número:");
+            num4 = int.Parse(Console.ReadLine());
+            Console.WriteLine("La resta de los números es: " + (num3 - num4));
+
+            // Multiplicación
+            int num5, num6;
+            Console.WriteLine("Introduzca un número:");
+            num5 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca otro número:");
+            num6 = int.Parse(Console.ReadLine());
+            Console.WriteLine("La multiplicación de los números es: " + (num5 * num6));
+
+            // División
+            int num7, num8;
+            Console.WriteLine("Introduzca un número:");
+            num7 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserte otro número:");
+            num8 = int.Parse(Console.ReadLine());
+            Console.WriteLine("La división de los números es: " + (num7 / num8));
+        }
+    }
+}
